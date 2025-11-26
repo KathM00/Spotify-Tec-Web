24f2d82d59b7be9a74e56cc6280da1865dd9e87e
Luiscuac <luiguiandre2208@gmail.com>
2025-11-26 18:27:54 -0400

    feat: Song Model

---
+++ Models/DTOS/CreateSongDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace spotify.Models.DTOS
+{
+    public class CreateSongDto
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int Duration { get; set; }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,11 @@
+﻿namespace spotify.Models
+{
+    public class Song
+    {
+        Guid id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+
+    }
+}
