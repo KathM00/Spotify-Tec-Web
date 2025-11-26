2ab2e824b46c00d4ce333b60a96dca8c1a90dbbb
Luiscuac <luiguiandre2208@gmail.com>
2025-11-26 19:14:46 -0400

    feat: DbContext Song & PlaylistSong

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,19 @@
-﻿namespace spotify.Data
+﻿using Microsoft.EntityFrameworkCore;
+using spotify.Models;
+
+namespace spotify.Data
 {
 
-    public class AppDbContext
+    public class AppDbContext : DbContext 
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Song> Songs => Set<Song>();
+        public DbSet<PlaylistSong> PlaylistSongs => Set<PlaylistSong>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+        }
     }
 }
