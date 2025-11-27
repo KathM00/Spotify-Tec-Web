d0ea7a8e344f695880c7e1a37b296ba1c627faf0
KathM00 <kathymontamejia@gmail.com>
2025-11-27 18:34:53 -0400

    feat: Adding Register Dto

---
+++ Models/DTOS/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace spotify.Models.DTOS
+{
+    public class RegisterDto
+    {
+        public string Username { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        public string Role { get; set; } = "User";
+    }
+}
