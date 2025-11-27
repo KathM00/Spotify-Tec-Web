3aa3b1703835e4a028a2ced9d326f330a9e8503d
Luiscuac <luiguiandre2208@gmail.com>
2025-11-27 00:02:07 -0400

    fix:song.cs

--- Models/Song.cs
+++ Models/Song.cs
@@ -3,10 +3,11 @@
     public class Song
     {
         public Guid id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } =default!;
         public int Duration { get; set; }
         public string Description { get; set; }
         public Guid ArtistId { get; set; }
+        public Artist Artist { get; set; } = default!;
         public ICollection <PlaylistSong> PlaylistSongs { get; set; } = new List<PlaylistSong>();
     }
 }
