9026882a0380ad2cfb77d0229be641a1e9e382a3
KathM00 <kathymontamejia@gmail.com>
2025-11-27 22:53:28 -0400

    Feat: Add IAuthService

---
+++ Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using spotify.Models.DTOS;
+
+namespace spotify.Services
+{
+    public interface IAuthService
+    {
+        Task<(bool ok, LoginResponseDto? response)> LoginAsync(LoginDto dto);
+        Task<bool> LogoutAsync(LogoutDto dto);
+        Task<string> RegisterAsync(RegisterDto dto);
+        Task<(bool ok, LoginResponseDto? response)> RefreshAsync(RefreshRequestDto dto);
+    }
+}
