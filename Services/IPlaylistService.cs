499d748ce770cd95abcb0da1defa14ee892b973a
KathM00 <kathymontamejia@gmail.com>
2025-11-27 23:33:10 -0400

    Fix: Fixing IPlaylistService

---
+++ Services/IPlaylistService.cs
@@ -0,0 +1,16 @@
+﻿using spotify.Models.DTOS;
+using spotify.Models;
+
+namespace spotify.Services
+{
+    public interface IPlaylistService
+    {
+        Task<IEnumerable<Playlist>> GetAll();
+        Task<Playlist> GetOne(Guid id);
+        Task<Playlist> Create(CreatePlaylistDto dto);
+        Task<Playlist> Update(UpdatePlaylistDto dto, Guid id);
+        Task Delete(Guid id);
+        Task AddSong(Guid playlistId, AddSongToPlaylistDto dto);
+        Task RemoveSong(Guid playlistId, Guid songId);
+    }
+}
