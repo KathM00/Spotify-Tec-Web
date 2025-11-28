2fdb803837220b77dd59d8017b018700725c5bd2
KathM00 <kathymontamejia@gmail.com>
2025-11-27 20:33:41 -0400

    fix: Playlist

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -6,7 +6,6 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Guid UserId { get; set; }
-        public bool IsPublic { get; set; }
         public User User { get; set; }
         public ICollection<PlaylistSong> PlaylistSongs { get; set; } = new List<PlaylistSong>();
     }
