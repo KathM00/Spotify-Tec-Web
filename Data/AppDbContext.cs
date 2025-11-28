9bb7151d55dbe66a05b5d39ceb71fb698503d7f1
KathM00 <kathymontamejia@gmail.com>
2025-11-28 00:40:27 -0400

    Fix: Fixing AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,30 +3,53 @@ using spotify.Models;
 
 namespace spotify.Data
 {
-
-    public class AppDbContext : DbContext 
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-        // Domain sets
-        public DbSet<Song> Songs => Set<Song>();
-        public DbSet<Artist> Artists => Set<Artist>();
-        public DbSet<Playlist> Playlists => Set<Playlist>();
-        public DbSet<User> Users => Set<User>();
-        public DbSet<ArtistProfile> ArtistProfiles => Set<ArtistProfile>();
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Artist> Artists { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Song -> Artist (many songs to one artist)
-            modelBuilder.Entity<Song>()
-                .HasOne(s => s.Artist)
-                .WithMany(a => a.Songs)
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Playlists)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+         
+            modelBuilder.Entity<Playlist>()
+                .HasMany(p => p.Songs)
+                .WithMany(s => s.Playlists)
+                .UsingEntity(j => j.ToTable("PlaylistSongs"));
+           
+            modelBuilder.Entity<Artist>()
+                .HasMany(a => a.Songs)
+                .WithOne(s => s.Artist)
                 .HasForeignKey(s => s.ArtistId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-  
+            modelBuilder.Entity<Playlist>()
+                .Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
 
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Song>()
+                .Property(s => s.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Username)
+                .HasMaxLength(50)
+                .IsRequired();
         }
     }
 }
